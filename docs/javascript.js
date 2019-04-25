e51be0a23ac73b512d468e2a9677e28700c3ff81
JulieSonne <jasonne@live.dk>
2019-04-25 13:59:50 +0200

    upload
    upload

--- docs/javascript.js
+++ docs/javascript.js
@@ -2,7 +2,6 @@
 let element = document.querySelector(".banner"); 
 element.addEventListener("load", setTimeout(reloadBanner, 3000)); //den starter med at tælle, når siden loader 
 
-
 function reloadBanner(){
 	location.reload();
 }
